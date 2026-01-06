8d1ff35f5651b602276cdd5c2f4e11930098a875
wasi479 <wasiosrs@gmail.com>
2026-01-06 08:20:51 +0500

    Update and rename login.html to login.jsx

---
+++ login.jsx
@@ -0,0 +1,28 @@
+import { Link, Router } from "react-router-dom";
+function Login() {
+    const submitHandler = () => {
+      alert("Login submitted ");
+    };
+  
+    return (
+      <section className="login-section">
+        <div className="login-container">
+          <h2>Welcome Back</h2>
+          <p>Login to manage your events</p>
+  
+          <form onSubmit={submitHandler}>
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <button className="btn">Login</button>
+  
+            <p>
+            Don’t have an account? <Link to="/signup">Register here</Link>
+            </p>
+          </form>
+        </div>
+      </section>
+    );
+  }
+  
+  export default Login;
+  
