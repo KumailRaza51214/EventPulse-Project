ee86f3045175e9eec7e0177c24054dc43e8107c1
wasi479 <wasiosrs@gmail.com>
2026-01-06 08:52:07 +0500

    Create footer.jsx

---
+++ footer.jsx
@@ -0,0 +1,26 @@
+  function Footer() {
+    return (
+      <footer className="footer">
+        <div className="footer-box">
+          <div>
+            <h3>EventPulse</h3>
+            <p>Discover events and connect with your community.</p>
+          </div>
+  
+          
+          <div>
+            <h4>Contact</h4>
+            <p>Email: info@eventpulse.com</p>
+            <p>Phone: +92 314-1111111</p>
+          </div>
+        </div>
+  
+        <p className="footer-bottom">
+          © 2025 EventPulse. All Rights Reserved.
+        </p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
